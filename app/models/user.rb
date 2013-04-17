8c4cb079b635f51a462028234b84ab0956c4529f
neetusharma <neetu.sharma@addvalsolutions.com>
2013-04-17 15:55:31 +0530

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :lockable, :timeoutable, :confirmable and :activatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :username, :attachment
+  attr_accessible :photo
+  has_attached_file :attachment
+
+  has_attached_file :photo, :styles => { :small => "150x150>" },
+                  :url  => "/assets/products/:id/:style/:basename.:extension",
+                  :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
+
+
+validates_attachment_size :photo, :less_than => 5.megabytes
+validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
+
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyReviews::Application.config.secret_token = '49faf3e9ae8a7496a850796b31ebd6a96d37e5091ecba2744f7ebd94275f0c4a869aff6679ace1de82d3c898678ed8c399e5b6998f4372aa1565e47ecdbac740'
